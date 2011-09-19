b362e491aa52f0a3ab26709f12b205fc6643c8c7
Benjamin Pavone <ben00ze@gmail.com>
2011-09-19 18:30:03 +0200

    Misha's commit

--- src/org/vamdc/kida/tap/Restrictables.java
+++ src/org/vamdc/kida/tap/Restrictables.java
@@ -14,38 +14,49 @@ import org.vamdc.dictionary.Restrictable;
 @SuppressWarnings("serial")
 public class Restrictables {
 
-
-	public static Collection<Restrictable> Restricts= new ArrayList<Restrictable>(){{
-		add(Restrictable.MoleculeIonCharge);
-		add(Restrictable.MoleculeChemicalName);
-		add(Restrictable.MoleculeInchi);
-		add(Restrictable.MoleculeInchiKey);
-	}};
-	
-
-	
+	public static Collection<Restrictable> Restricts = new ArrayList<Restrictable>() {
+		{
+			add(Restrictable.MoleculeIonCharge);
+			add(Restrictable.MoleculeChemicalName);
+			add(Restrictable.MoleculeInchi);
+			add(Restrictable.MoleculeInchiKey);
+		}
+	};
 
 	/**
-	 * Species paths 
+	 * Species paths
 	 * */
-	public final static Map<Restrictable,String> SpeciesPathSpec = new HashMap<Restrictable,String>(){{
-		put(Restrictable.MoleculeIonCharge,"charge");
-		put(Restrictable.MoleculeChemicalName,"commonName");
-		put(Restrictable.MoleculeInchi,"inchi");
-		put(Restrictable.MoleculeInchiKey,"inchiKey");
-		
-	}};
-	
+	public final static Map<Restrictable, String> SpeciesPathSpec = new HashMap<Restrictable, String>() {
+		{
+			put(Restrictable.MoleculeIonCharge, "charge");
+			put(Restrictable.MoleculeChemicalName, "commonName");
+			put(Restrictable.MoleculeInchi, "inchi");
+			put(Restrictable.MoleculeInchiKey, "inchiKey");
+
+		}
+	};
+
 	/**
 	 * Channel paths
 	 */
-	public final static Map<Restrictable,String> ChannelPathSpec = new HashMap<Restrictable,String>(){{
-		put(Restrictable.MoleculeIonCharge,"specieRel.charge");
-		put(Restrictable.MoleculeChemicalName,"specieRel.commonName");
-		put(Restrictable.MoleculeInchi,"specieRel.inchi");
-		put(Restrictable.MoleculeInchiKey,"specieRel.inchiKey");
-	}};
-	
-	
-	
+	public final static Map<Restrictable, String> ChannelPathSpec = new HashMap<Restrictable, String>() {
+		{
+			put(Restrictable.MoleculeIonCharge, "specieRel.charge");
+			put(Restrictable.MoleculeChemicalName, "specieRel.commonName");
+			put(Restrictable.MoleculeInchi, "specieRel.inchi");
+			put(Restrictable.MoleculeInchiKey, "specieRel.inchiKey");
+		}
+	};
+
+	public static Map<Restrictable, String> getAliasedChannelMap(String prefix) {
+		Map<Restrictable, String> cps = new HashMap<Restrictable, String>();
+		if (prefix != null) {
+			cps.put(Restrictable.MoleculeIonCharge, prefix + ".charge");
+			cps.put(Restrictable.MoleculeChemicalName, prefix + ".commonName");
+			cps.put(Restrictable.MoleculeInchi, prefix + ".inchi");
+			cps.put(Restrictable.MoleculeInchiKey, prefix + ".inchiKey");
+		}
+		return cps;
+	}
+
 }
